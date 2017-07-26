266102b61b80800ff425aadaa42adb8b785e3e5f
Steven Gordon <s.d.gordon@cqu.edu.au>
2017-07-27 08:10:29 +1000

    Coied inheritance from textbook

---
+++ tutorial/steve/2017t2/inheritance/src/inheritance/ManagerTest.java
@@ -0,0 +1,55 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Steven Gordon <s.d.gordon@cqu.edu.au>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package inheritance;
+
+/**
+ *
+ * @author Steven Gordon <s.d.gordon@cqu.edu.au>
+ */
+/**
+ * This program demonstrates inheritance.
+ * @version 1.21 2004-02-21
+ * @author Cay Horstmann
+ */
+public class ManagerTest
+{
+   public static void main(String[] args)
+   {
+      // construct a Manager object
+      Manager boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+      boss.setBonus(5000);
+
+      Employee[] staff = new Employee[3];
+
+      // fill the staff array with Manager and Employee objects
+
+      staff[0] = boss;
+      staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+      staff[2] = new Employee("Tommy Tester", 40000, 1990, 3, 15);
+
+      // print out information about all Employee objects
+      for (Employee e : staff)
+         System.out.println("name=" + e.getName() + ",salary=" + e.getSalary());
+   }
+}
