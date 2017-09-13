40e83f4ef07bab26432b595bc09f211e4b1e5b32
gordonds <gordonds@CQUCRNT18541.staff.ad.cqu.edu.au>
2017-09-13 15:36:33 +1000

    Generics and pairs

--- corejava/v1ch08/pair3/Pair.java
+++ corejava/v1ch08/pair3/Pair.java
@@ -4,17 +4,34 @@ package pair3;
  * @version 1.00 2004-05-10
  * @author Cay Horstmann
  */
-public class Pair<T> 
-{
-   private T first;
-   private T second;
+public class Pair<T> {
 
-   public Pair() { first = null; second = null; }
-   public Pair(T first, T second) { this.first = first;  this.second = second; }
+    private T first;
+    private T second;
 
-   public T getFirst() { return first; }
-   public T getSecond() { return second; }
+    public Pair() {
+        first = null;
+        second = null;
+    }
 
-   public void setFirst(T newValue) { first = newValue; }
-   public void setSecond(T newValue) { second = newValue; }
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setFirst(T newValue) {
+        first = newValue;
+    }
+
+    public void setSecond(T newValue) {
+        second = newValue;
+    }
 }
