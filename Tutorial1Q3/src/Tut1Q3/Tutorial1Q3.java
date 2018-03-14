7ab1fe5c9d1ba90363fc7b9f35884518fd511f52
12067163 <12067163@138.77.180.62>
2018-03-14 17:48:35 +1000

    Work dagnabbit

---
+++ Tutorial1Q3/src/Tut1Q3/Tutorial1Q3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tut1Q3;
+
+import java.awt.FlowLayout;
+import javax.swing.*;
+/**
+ *
+ * @author John Barber (john.barber@cqumail.com)
+ */
+public class Tutorial1Q3 extends JFrame 
+{
+    private JLabel lblRadius = new JLabel();
+    Tutorial1Q3()
+    {
+        super("Calculate Circle Area");
+        this.setBounds(400, 400, 400, 400);
+        
+        
+        lblRadius.        
+        
+        setLayout(new FlowLayout());
+        this.setVisible(true);
+    }
+    
+    
+  
+}
