208844fde14bda00fd41d430f991dcdd6c2d2102
12061879 <12061879@138.77.180.70>
2018-03-28 17:40:24 +1000

    WEEK 4 GUI PLAY AROUND

--- tutorial/douglas/GuiPlay/src/guiplay/newpackage/MyFrame.java
+++ tutorial/douglas/GuiPlay/src/guiplay/newpackage/MyFrame.java
@@ -7,10 +7,14 @@ package guiplay.newpackage;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 /**
  *
@@ -24,37 +28,121 @@ public class MyFrame extends JFrame {
     private JButton JB1;
     private JButton JB2;
     private JButton JB3;
+    private JButton JB4; 
+   
     
     private JPanel JP1;
     private JPanel JP2; 
+    private JPanel JP3;
+    private JPanel JP4; 
+    private JPanel JP5;
+    
+    private JComboBox JComBox;
+    private JCheckBox JCheckBox1; 
+    private JCheckBox JCheckBox2;
+    private JCheckBox JCheckBox3;
+    private JCheckBox JCheckBox4;
+    
+    private JRadioButton JRadioButton1; 
+    private JRadioButton JRadioButton2; 
+    private JRadioButton JRadioButton3; 
+    
+    private ButtonGroup ButtonGroup1; 
     
     public MyFrame(){
-        JL1 = new JLabel ("Good Day,");
-        JL2 = new JLabel ("How's it going mate");
         
-        JB1 = new JButton ("Click this");
-        JB2 = new JButton ("Don't click this");
-        JB3 = new JButton ("No seriously, don't click this one");
+        
+        
+        JL1 = new JLabel ("Print: My Printer");
+        JL2 = new JLabel ("Printer Quality");
+        
+        JB1 = new JButton ("OK");
+        JB2 = new JButton ("Cancel");
+        JB3 = new JButton ("Setup...");
+        JB4 = new JButton("Help"); 
         
         JP1 = new JPanel (); 
         JP2 = new JPanel (); 
+        JP3 = new JPanel ();
+        JP4 = new JPanel ();
+        JP5 = new JPanel (); 
         
-        setLayout (new GridLayout(2,1));
-        JP1.setLayout(new BorderLayout());
+        JComBox = new JComboBox();
+        JCheckBox1 = new JCheckBox("Image"); 
+        JCheckBox2 = new JCheckBox("Text"); 
+        JCheckBox3 = new JCheckBox("Code"); 
+        JCheckBox4 = new JCheckBox("Print to file"); 
+        
+        JRadioButton1 = new JRadioButton("Selection"); 
+        JRadioButton2 = new JRadioButton("All"); 
+        JRadioButton3 = new JRadioButton("Applet"); 
+        
+        ButtonGroup1 = new ButtonGroup (); 
         
         
+        
+        setLayout (new BorderLayout());
+        
+        
+        
+        JP2.setLayout (new GridLayout(4,1)); //EAST 
+        JP1.setLayout (new BorderLayout()); //CENTER
+        
+        JP1.add(JP3, BorderLayout.NORTH); //NORTH WITHIN JP1
+        JP1.add(JP4, BorderLayout.CENTER);//CENTER WITHIN JP1
+        JP1.add(JP5, BorderLayout.SOUTH);//SOUTH WITHIN JP1
+        
+        JP2.add(JB1); //GRID LAYOUT - Button "OK"
+        JP2.add(JB2); //GRID LAYOUT - Button "Cancel"
+        JP2.add(JB3);//GRID LAYOUT - Button "Setup.."
+        JP2.add(JB4); //GRID LAYOUT - Button "Help"
+        
+        JP3.add(JL1);//NORTH WITHIN JP1 -Label "Print: My Printer";
+        JP5.add(JL2);////SOUTH WITHIN JP1 - "Printer Quality"
+        
+        JComBox.addItem("High"); 
+        JComBox.addItem("Low"); 
+        JP5.add(JComBox); 
+        
+        JP4.add(JCheckBox1); 
+        JP4.add(JCheckBox2); 
+        JP4.add(JCheckBox3); 
+        JP5.add(JCheckBox4); 
+        
+        
+        ButtonGroup1.add(JRadioButton1); 
+        ButtonGroup1.add(JRadioButton2);
+        ButtonGroup1.add(JRadioButton3); 
+        
+        
+        
+        JP4.add(JRadioButton1); 
+        JP4.add(JRadioButton2);
+        JP4.add(JRadioButton3); 
+        
+        
+        add(JP1,BorderLayout.CENTER); 
+        add(JP2,BorderLayout.EAST); 
+        
+        
+        
+        
+        
+        
+         
+                
+        
+        
+       //JP1.add(JB1, BorderLayout.EAST);
+        //JP1.add(JL1, BorderLayout.CENTER); 
+        
+        
+       
       
-       //this.add(JL1,BorderLayout.NORTH); 
-        //this.add(JL2, BorderLayout.SOUTH); 
+       
         
-        //this.add(JB1, BorderLayout.WEST);
-        //this.add(JB2, BorderLayout.CENTER);
-        //this.add(JB3, BorderLayout.EAST);
-        
-        JP1.add(JL1, BorderLayout.CENTER);
-        JP1.add(JL2,BorderLayout.NORTH);
-        JP1.add(JB1,BorderLayout.SOUTH);
-        this.add(JP1);
+       
+
         
         
         
