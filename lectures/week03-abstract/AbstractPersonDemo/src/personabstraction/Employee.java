954d7a8414b18807c2734961d86719a7400f9a5b
sgordon <sgordon@chilli>
2018-07-23 16:50:20 +1000

    Vehicle tutorial for week 2

---
+++ lectures/week03-abstract/AbstractPersonDemo/src/personabstraction/Employee.java
@@ -0,0 +1,134 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Steven Gordon <s.d.gordon@cqu.edu.au>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package personabstraction;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Steven Gordon <s.d.gordon@cqu.edu.au>
+ */
+public class Employee {
+
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee() {
+        this.name = "";
+        this.salary = 0.0;
+        this.hireDay = LocalDate.of(2018, 7, 9);
+    }
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = LocalDate.of(2018, 7, 9);
+
+    }
+
+    public Employee(String name, double salary, int year, int month, int day) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = LocalDate.of(year, month, day);
+    }
+
+    /**
+     * Get the value of hireDay
+     *
+     * @return the value of hireDay
+     */
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    /**
+     * Set the value of hireDay
+     *
+     * @param hireDay new value of hireDay
+     */
+    public void setHireDay(LocalDate hireDay) {
+        this.hireDay = hireDay;
+    }
+
+    /**
+     * Set the value of hireDay
+     *
+     * @param year year of hire
+     * @param month month of hire
+     * @param day day of hire
+     */
+    public void setHireDay(int year, int month, int day) {
+        this.hireDay = LocalDate.of(year, month, day);
+    }
+
+    /**
+     * Get the value of salary
+     *
+     * @return the value of salary
+     */
+    public double getSalary() {
+        return salary;
+    }
+
+    /**
+     * Set the value of salary
+     *
+     * @param salary new value of salary
+     */
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void raiseSalary(double byPercent) {
+        this.salary = this.salary + this.salary * byPercent / 100;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "name: " + name + "; salary: " + salary + "; hireDay:" + hireDay + '}';
+    }
+
+    
+    
+
+}
---
+++ lectures/week03-abstract/AbstractPersonDemo/src/personabstraction/EmployeeTester.java
@@ -0,0 +1,58 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Steven Gordon <s.d.gordon@cqu.edu.au>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package personabstraction;
+
+/**
+ *
+ * @author Steven Gordon <s.d.gordon@cqu.edu.au>
+ */
+public class EmployeeTester {
+    public static void main(String[] args) {
+        Employee e1;
+        e1 = new Employee("Steve", 100000.0);
+        
+        Employee e2;
+        e2 = new Employee("Jamie", 120000.0, 2016, 1, 31);
+        
+        Manager m1;
+        m1 = new Manager("Col", 200000.0, 1981, 12, 25, 35000.0);
+        
+        //System.out.println(e1.getName() + " " + e1.getSalary());
+        //System.out.println(e2.getName() + " " + e2.getSalary());
+        //System.out.println(m1.getName() + " " + m1.getSalary() + " " + m1.getBonus());
+
+        Employee[] staffList = new Employee[3];
+        staffList[0] = new Employee("Steve", 100000.0);
+        staffList[1] = new Employee("Jamie", 120000.0, 2016, 1, 31);
+        staffList[2] = new Manager("Col", 200000.0, 1981, 12, 25, 35000.0);
+        
+        for (Employee emp : staffList) {
+            System.out.println(emp.getName() + " " + emp.getSalary());
+        }
+        
+        System.out.println(m1);
+        
+    }
+    
+}
