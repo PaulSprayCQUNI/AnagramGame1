f3e6e7fb6e69a956d6ef60259cb905a91d88fd38
PaulSpray <paul.spray@cqumail.com>
2019-03-29 14:26:55 +1000

    new commit - basic GUI learning

---
+++ FoodGUI/src/foodgui/Apples.java
@@ -0,0 +1,32 @@
+/*
+ *the simplest of GUIs.
+ */
+package foodgui;
+
+/**
+ *
+ * @author 12084819
+ */
+
+import javax.swing.JOptionPane;
+
+public class Apples {
+/*    
+    public static void main(String[] args) {
+        
+        String firstNumber = JOptionPane.showInputDialog ("Enter first number: ");
+        String secondNumber = JOptionPane.showInputDialog ("Enter second number: ");
+        
+        //convert string variables to intergers
+        
+        int num1 = Integer.parseInt(firstNumber);
+        int num2 = Integer.parseInt(secondNumber);
+        int sum = num1 + num2;
+        
+        JOptionPane.showMessageDialog(null, "The answer is " + sum, 
+                "add it up", JOptionPane.PLAIN_MESSAGE);                // null is middle of screen position, "" is the text, next is the title, last is the set-up of the pain
+    }*/
+    
+    
+    
+}
---
+++ FoodGUI/src/foodgui/Tuna.java
@@ -0,0 +1,16 @@
+/*
+ * the next level of simplest of GUIs - GUI with JFrame
+ */
+package foodgui;
+
+/**
+ *
+ * @author 12084819
+ */
+
+import java.awt.FlowLayout;             // runs the layout from left to right, until out of room
+import javax.swing.JFrame;
+
+public class Tuna {
+    
+}
