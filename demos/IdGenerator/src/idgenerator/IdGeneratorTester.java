bd9b2d9db577f9bea0ef8cc2e9803dbecefea6b8
Steven Gordon <s.d.gordon@cqu.edu.au>
2019-04-12 17:40:22 +1000

    Simple example of Id generator

---
+++ demos/IdGenerator/src/idgenerator/IdGeneratorTester.java
@@ -0,0 +1,40 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 Steven Gordon <s.d.gordon@cqu.edu.au>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package idgenerator;
+
+/**
+ *
+ * @author Steven Gordon <s.d.gordon@cqu.edu.au>
+ */
+public class IdGeneratorTester {
+
+    public static void main(String[] args) {
+        System.out.println(IdGenerator.getNextStudentId());
+        System.out.println(IdGenerator.getNextTeacherId());
+        System.out.println(IdGenerator.getNextStudentId());
+        System.out.println(IdGenerator.getNextTeacherId());
+        
+    }
+
+}
