ddd15d136a67c1944140d03a96a15771e1a9552b
PaulSpray <paul.spray@cqumail.com>
2019-04-12 12:28:38 +1000

    Week 5 Assignment1 Work- oop

--- FSA App/src/FSAApp/FSAAppFrame.java
+++ FSA App/src/FSAApp/FSAAppFrame.java
@@ -12,6 +12,10 @@ package FSAApp;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.*;
 
 public class FSAAppFrame extends JFrame {
     
@@ -20,30 +24,35 @@ public class FSAAppFrame extends JFrame {
     private JButton displayRentalBtn;
     private JTextField textId;
     private JPanel pnlMainBtns;
-    private JPanel pnlIdText;
+    private JPanel pnlMainEdit;
 
-    
+                                    // to do - add an array[] to store details for Rental class, not an array list (assignment 2 only?)
+                                    // to do - intialise the array within constructor below     
     
     public FSAAppFrame(){
         
-        this.setLayout(new BorderLayout());
+        // add components for FSAAppFrame - frame and layout
         
-        enterRentalBtn = new JButton("Enter");
+        pnlMainBtns = new JPanel();
+        pnlMainEdit = new JPanel();
+        this.setLayout(new BorderLayout());
+        //construct buttons and text field                            
+        enterRentalBtn = new JButton("Enter");      
         editRentalBtn = new JButton("Edit");
         displayRentalBtn = new JButton("Display");
-        textId = new JTextField("Enter Rental Id");
-        pnlIdText = new JPanel();
-        pnlMainBtns = new JPanel();
+        textId = new JTextField("Enter Rental Id of Rental to Edit");
         
+        // add buttons to panel pnlMainBtns, JTextField text Id to  pnlIdText, and pnlMainBts + pnlIdText to frame
         pnlMainBtns.add(enterRentalBtn);
-        pnlMainBtns.add(editRentalBtn);
         pnlMainBtns.add(displayRentalBtn);
-        pnlIdText.add(textId);
-        pnlMainBtns.setLayout(new GridLayout(3,1));
-        pnlIdText.setLayout(new GridLayout(0,1)); 
-                
-        this.add(pnlMainBtns, BorderLayout.CENTER);
-        this.add(pnlIdText,BorderLayout.EAST);
+        pnlMainEdit.add(editRentalBtn);
+        pnlMainEdit.add(textId);
+        //pnlMainBtns.setLayout(new GridLayout(3,1));
+        //pnlMainEdit.setLayout(new GridLayout(0,1)); 
+        
+              
+        this.add(pnlMainBtns, BorderLayout.NORTH);
+        this.add(pnlMainEdit);//,BorderLayout.CENTER);
         //this.add(BorderLayout.NORTH);
         //this.add(BorderLayout.WEST);
         //this.add(BorderLayout.SOUTH);
